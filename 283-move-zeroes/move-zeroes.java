d0a357a1a49cda263bb72caf3d4d816c0b7e5c20
Bharath <alli123uma@gmail.com>
2026-01-25 12:08:22 +0530

    Time: 3 ms (20.05%) | Memory: 48 MB (13.14%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[i]=nums[i]+nums[j]-(nums[j]=nums[i]);
+                j++;
+            }
+        }
+    }
+}

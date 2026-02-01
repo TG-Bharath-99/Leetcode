ce76e0fc534d82ec306ae7cd5b902ee7292111b3
Bharath <alli123uma@gmail.com>
2026-02-01 19:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (90.07%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -21,6 +21,6 @@ class Solution{
         if(root.val==targetSum && root.left==null && root.right==null){
             return true;
         }
-       return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
     }
 }

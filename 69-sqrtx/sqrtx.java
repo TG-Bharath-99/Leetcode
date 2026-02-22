e6d2d5e7d55aeec6973d32be6ec5650e780e206b
Bharath <alli123uma@gmail.com>
2026-02-22 13:38:20 +0530

    Time: 1 ms (96.97%) | Memory: 42.7 MB (61.88%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,20 @@
+class Solution{
+    public int mySqrt(int x){
+        int l=1,h=x;
+        int ans=0;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(mid==x/mid){
+                return mid;
+            }
+            else if(mid<x/mid){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return ans;
+    }
+}

1109421c725100457737668bf8736342902cfa0a
TG-Bharath-99 <ummadibharath07@gmail.com>
2026-03-02 17:19:50 +0530

    updtated one line in one file

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,9 +1,10 @@
 import java.math.BigInteger;
 class Solution {
     public String multiply(String num1, String num2){
-        BigInteger a=new BigInteger(num1); //Hello
+        BigInteger a=new BigInteger(num1); 
         BigInteger b=new BigInteger(num2);
         BigInteger result=a.multiply(b);
+        System.out.print("Hello");
         return String.valueOf(result);
     }
 }

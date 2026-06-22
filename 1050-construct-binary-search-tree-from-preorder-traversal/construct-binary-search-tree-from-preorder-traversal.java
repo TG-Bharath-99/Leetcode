d475de23b5354a4548badf5fcbdb7f36d39d9951
Bharath <bharathummadi4@gmail.com>
2026-06-22 15:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (39.91%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -16,19 +16,13 @@
 class Solution{
     int index=0;
     public TreeNode bstFromPreorder(int[] preorder){
-        if(preorder.length==0){
-            return null;
-        }
         return helper(preorder,Integer.MAX_VALUE);
     }
-    TreeNode helper(int []preorder,int bound){
-        if(index==preorder.length || preorder[index]>bound){
-            return null;
-        }
-        int val=preorder[index++];
-        TreeNode node=new TreeNode(val);
-        node.left=helper(preorder,val);
-        node.right=helper(preorder,bound);
-        return node;
+    public TreeNode helper(int []preorder,int bound){
+        if(preorder.length==index || preorder[index]>bound) return null;
+        TreeNode root=new TreeNode(preorder[index++]);
+        root.left=helper(preorder,root.val);
+        root.right=helper(preorder,bound);
+        return root;
     }
 }

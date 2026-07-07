fca58d3d4e2e05f90e32cdbb3c1028108ff0ffb6
Bharath <bharathummadi4@gmail.com>
2026-07-07 22:15:31 +0530

    Time: 92 ms (63.26%), Space: 281.6 MB (58.39%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,32 +1,27 @@
 class Solution{
-    public void convertToList(List<List<Integer>>graph,int n,int [][]edges){
+    public void convertToList(int [][]edges,int n,List<List<Integer>>graph){
         for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
         for(int i=0;i<edges.length;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            graph.get(edges[i][0]).add(edges[i][1]);
+            graph.get(edges[i][1]).add(edges[i][0]);
         }
     }
     public boolean validPath(int n, int[][] edges, int source, int destination){
-        if(source==destination) return true;
         List<List<Integer>>graph=new ArrayList<>();
-        convertToList(graph,n,edges);
-        boolean []visited=new boolean[n];
+        convertToList(edges,n,graph);
         Queue<Integer>q=new LinkedList<>();
-        visited[source]=true;
+        boolean []visited=new boolean[n];
         q.offer(source);
+        visited[source]=true;
         while(!q.isEmpty()){
             int curr=q.poll();
+            if(curr==destination) return true;
             for(int num : graph.get(curr)){
-                if(num==destination){
-                    return true;
-                }
                 if(!visited[num]){
-                    visited[num]=true;
                     q.offer(num);
+                    visited[num]=true;
                 }
             }
         }

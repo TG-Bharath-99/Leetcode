cc21ec467cc3ffdf2d094b5ff40fc308d2c5fd35
Bharath <bharathummadi4@gmail.com>
2026-08-13 20:09:34 +0530

    Time: 1 ms (38.70%) | Memory: 46.9 MB (43.74%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,28 @@
 class Solution{
-    public void fill(int[][] image, int sr, int sc,int originalcolor,int color){
-        if(image[sr][sc]==color) return;
+    void fill(int [][]image,int sr,int sc,int originalcolor,int color,boolean [][]visited){
+        if(visited[sr][sc]) return;
+        image[sr][sc]=color;
+        visited[sr][sc]=true;
         int m=image.length;
         int n=image[0].length;
-        image[sr][sc]=color;
-        if(sr-1>=0 && sc<n && originalcolor==image[sr-1][sc]){
-            fill(image,sr-1,sc,originalcolor,color);
+        if(sr-1>=0 && sc<n && image[sr-1][sc]==originalcolor){
+            fill(image,sr-1,sc,originalcolor,color,visited);
         }
-        if(sr+1<m && sc<n && originalcolor==image[sr+1][sc]){
-            fill(image,sr+1,sc,originalcolor,color);
+        if(sr+1<m && sc<n && image[sr+1][sc]==originalcolor){
+            fill(image,sr+1,sc,originalcolor,color,visited);
         }
-        if(sr<m && sc-1>=0 && originalcolor==image[sr][sc-1]){
-            fill(image,sr,sc-1,originalcolor,color);
+        if(sc-1>=0 && sr<m && image[sr][sc-1]==originalcolor){
+            fill(image,sr,sc-1,originalcolor,color,visited);
         }
-        if(sr<m && sc+1<n && originalcolor==image[sr][sc+1]){
-            fill(image,sr,sc+1,originalcolor,color);
+        if(sc+1<n && sr<m && image[sr][sc+1]==originalcolor){
+            fill(image,sr,sc+1,originalcolor,color,visited);
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color){
         int originalcolor=image[sr][sc];
-        if(originalcolor==color) return image;
-        fill(image,sr,sc,originalcolor,color);
+        if(image[sr][sc]==color) return image;
+        boolean [][]visited=new boolean[image.length][image[0].length];
+        fill(image,sr,sc,originalcolor,color,visited);
         return image;
     }
 }

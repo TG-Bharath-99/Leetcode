8d2fabbfb31fc290cb16377871df44d4a291c364
Bharath <bharathummadi4@gmail.com>
2026-08-13 19:48:02 +0530

    Time: 57 ms (85.08%), Space: 281.9 MB (45.25%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,5 +1,6 @@
 class Solution{
-    public void convertToList(int [][]edges,int n,List<List<Integer>>graph){
+    public boolean validPath(int n, int[][] edges, int source, int destination){
+        List<List<Integer>>graph=new ArrayList<>();
         for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
@@ -7,21 +8,17 @@ class Solution{
             graph.get(edges[i][0]).add(edges[i][1]);
             graph.get(edges[i][1]).add(edges[i][0]);
         }
-    }
-    public boolean validPath(int n, int[][] edges, int source, int destination){
-        List<List<Integer>>graph=new ArrayList<>();
-        convertToList(edges,n,graph);
         Queue<Integer>q=new LinkedList<>();
         boolean []visited=new boolean[n];
-        q.offer(source);
         visited[source]=true;
+        q.offer(source);
         while(!q.isEmpty()){
             int curr=q.poll();
             if(curr==destination) return true;
-            for(int num : graph.get(curr)){
-                if(!visited[num]){
-                    q.offer(num);
-                    visited[num]=true;
+            for(int nei : graph.get(curr)){
+                if(!visited[nei]){
+                    q.offer(nei);
+                    visited[nei]=true;
                 }
             }
         }

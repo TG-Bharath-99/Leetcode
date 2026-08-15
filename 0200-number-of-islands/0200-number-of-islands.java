b98cd151c2827fd35edfd690f61b61cdcf838bc3
Bharath <bharathummadi4@gmail.com>
2026-08-16 00:22:09 +0530

    Time: 4 ms (45.87%), Space: 52.5 MB (33.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,20 +1,14 @@
 class Solution{
     int count=0;
-    public void visit(char [][]grid,int row,int col,boolean [][]visited){
+    void visit(char [][]grid,int row,int col,boolean [][]visited){
         if(visited[row][col]) return;
         visited[row][col]=true;
-        if(row-1>=0 && col<grid[0].length && grid[row-1][col]=='1'){
-            visit(grid,row-1,col,visited);
-        }
-        if(row+1<grid.length && col<grid[0].length && grid[row+1][col]=='1'){
-            visit(grid,row+1,col,visited);
-        }
-        if(row<grid.length && col-1>=0 && grid[row][col-1]=='1'){
-            visit(grid,row,col-1,visited);
-        }
-        if(row<grid.length && col+1<grid[0].length && grid[row][col+1]=='1'){
-            visit(grid,row,col+1,visited);
-        }
+        int m=grid.length;
+        int n=grid[0].length;
+        if(row-1>=0 && grid[row-1][col]=='1') visit(grid,row-1,col,visited);
+        if(row+1<m && grid[row+1][col]=='1') visit(grid,row+1,col,visited);
+        if(col-1>=0 && grid[row][col-1]=='1') visit(grid,row,col-1,visited);
+        if(col+1<n && grid[row][col+1]=='1') visit(grid,row,col+1,visited);
     }
     public int numIslands(char[][] grid){
         boolean [][]visited=new boolean[grid.length][grid[0].length];

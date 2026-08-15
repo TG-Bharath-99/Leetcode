c44b9807477b2c2cf6e1e5ea3e711fa51c788deb
Bharath <bharathummadi4@gmail.com>
2026-08-16 00:12:42 +0530

    Time: 1 ms (82.56%), Space: 46.1 MB (92.41%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,18 +1,16 @@
 class Solution{
-    public void visit(List<List<Integer>> rooms,int key,boolean []visited){
+    void visit(List<List<Integer>> rooms,int key,boolean []visited){
         visited[key]=true;
-        for(int nextkey : rooms.get(key)){
-            if(!visited[nextkey]){
-                visit(rooms,nextkey,visited);
+        for(int nei : rooms.get(key)){
+            if(!visited[nei]){
+                visit(rooms,nei,visited);
             }
         }
     }
     public boolean canVisitAllRooms(List<List<Integer>> rooms){
         boolean []visited=new boolean[rooms.size()];
         visit(rooms,0,visited);
-        for(boolean b : visited){
-            if(!b) return false;
-        }
+        for(boolean b : visited) if(!b) return false;
         return true;
     }
 }

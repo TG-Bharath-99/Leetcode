a12150f0d9f38bf1c62b9b14daccf91ba58f1969
Bharath <bharathummadi4@gmail.com>
2026-08-23 12:05:21 +0530

    Time: 9 ms (29.93%), Space: 46.6 MB (94.89%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,14 @@
+class Solution{
+    public boolean sumGame(String num){
+        int sum1=0,sum2=0,q1=0,q2=0;
+        for(int i=0;i<num.length()/2;i++){
+            if(num.charAt(i)=='?') q1++;
+            else sum1+=num.charAt(i)-'0';
+        }
+        for(int i=num.length()/2;i<num.length();i++){
+            if(num.charAt(i)=='?') q2++;
+            else sum2+=num.charAt(i)-'0';
+        }
+        return 2*(sum1-sum2)!=9*(q2-q1);
+    }
+}

4239e971ca56d8c12bf197698ec1e76f42ff15be
Bharath <bharathummadi4@gmail.com>
2026-08-30 00:08:08 +0530

    Time: 53 ms (15.58%) | Memory: 50.2 MB (57.13%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,11 +1,8 @@
 class Solution{
-    public int subarraysWithKDistinct(int[] nums, int k){
-        return atMost(nums,k)-atMost(nums,k-1);
-    }
-    public int atMost(int []nums,int k){
-        int count=0;
-        int left=0;
+    int atmost(int []nums,int k){
         Map<Integer,Integer>map=new HashMap<>();
+        int left=0;
+        int ans=0;
         for(int right=0;right<nums.length;right++){
             map.put(nums[right],map.getOrDefault(nums[right],0)+1);
             while(map.size()>k){
@@ -15,9 +12,11 @@ class Solution{
                 }
                 left++;
             }
-            
-                count+=right-left+1;
+            ans+=right-left+1;
         }
-        return count;
+        return ans;
+    }
+    public int subarraysWithKDistinct(int[] nums, int k){
+        return atmost(nums,k)-atmost(nums,k-1);
     }
 }

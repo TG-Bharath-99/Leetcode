0b534c27ded9d3f63313e33eba05f535dc709a52
Bharath <bharathummadi4@gmail.com>
2026-09-03 23:54:39 +0530

    Time: 5 ms (83.33%) | Memory: 121.3 MB (84.78%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,13 @@
+class Solution{
+    public boolean uniformArray(int[] nums1){
+        int min=nums1[0];
+        boolean allEven=true;
+        for(int i : nums1){
+            min=Math.min(min,i);
+            if(i%2!=0){
+                allEven=false;
+            }
+        }
+        return min%2!=0 || allEven;
+    }
+}

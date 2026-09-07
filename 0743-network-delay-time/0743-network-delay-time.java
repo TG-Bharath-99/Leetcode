b9d212982545be6d2d4b2114717c83b39719cc30
Bharath <bharathummadi4@gmail.com>
2026-09-08 00:13:15 +0530

    Time: 17 ms (19.79%), Space: 48.4 MB (94.65%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,42 +1,40 @@
 class Solution{
-    public void convert(int [][]times,int n,List<List<int[]>>graph){
+    void convert(int [][]times,int n,List<List<int[]>>graph){
         for(int i=0;i<=n;i++) graph.add(new ArrayList<>());
-        for(int []edge : times){
-            int u=edge[0];
-            int v=edge[1];
-            int w=edge[2];
+        for(int i=0;i<times.length;i++){
+            int u=times[i][0];
+            int v=times[i][1];
+            int w=times[i][2];
             graph.get(u).add(new int[]{v,w});
         }
     }
     public int networkDelayTime(int[][] times, int n, int k){
-        int []dis=new int[n+1];
-        int count=-1;
         List<List<int[]>>graph=new ArrayList<>();
         convert(times,n,graph);
-        for(int i=1;i<=n;i++) dis[i]=Integer.MAX_VALUE;
-        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
+        int []dis=new int[n+1];
+        for(int i=0;i<n+1;i++) dis[i]=Integer.MAX_VALUE;
         dis[k]=0;
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
         pq.offer(new int[]{k,0});
         while(!pq.isEmpty()){
-                int []curr=pq.poll();
-                int node=curr[0];
-                int currdis=curr[1];
-                if(currdis>dis[node]) continue;
-                for(int []nei : graph.get(node)){
-                    int neinode=nei[0];
-                    int neidis=nei[1];
-                    if(neidis+dis[node]<dis[neinode]){
-                        dis[neinode]=neidis+dis[node];
-                    
-                    pq.offer(new int[]{neinode,dis[neinode]});}
+            int []curr=pq.poll();
+            int node=curr[0];
+            int cost=curr[1];
+            if(cost>dis[node]) continue;
+            for(int []nei : graph.get(node)){
+                int newnode=nei[0];
+                int newcost=nei[1];
+                if(newcost+dis[node]<dis[newnode]){
+                    dis[newnode]=dis[node]+newcost;
+                    pq.offer(new int[]{newnode,dis[newnode]});
                 }
-           
+            }
         }
-        int max=-999;
-        for(int i=1;i<dis.length;i++){
+        int ans=-1;
+        for(int i=1;i<=n;i++){
             if(dis[i]==Integer.MAX_VALUE) return -1;
-            max=Math.max(max,dis[i]);
+            ans=Math.max(ans,dis[i]);
         }
-        return max;
+        return ans;
     }
 }

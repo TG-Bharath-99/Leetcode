7ccfa25f83f6f40beaa2184170f8f7e52294608b
Bharath <bharathummadi4@gmail.com>
2026-09-07 23:46:07 +0530

    Time: 0 ms (100%), Space: 42.8 MB (52.25%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,8 +13,8 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+class Solution{
+    public boolean isSameTree(TreeNode p, TreeNode q){
         if(p==null && q==null){
             return true;
         }

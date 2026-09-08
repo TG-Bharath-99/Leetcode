ac429b1222271381f841540cb608df5079299097
Bharath <bharathummadi4@gmail.com>
2026-09-08 22:08:36 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,15 +1,6 @@
 class Solution{
     public int countCommas(int n){
-        int ans=0;
-        if(n>=1000){
-            ans+=n-999;
-        }
-        if(n>=1000000){
-            ans+=n-99999;
-        }
-        if(n>=1000000000){
-            ans+=n-99999999;
-        }
-        return ans;
+        if(n<1000) return 0;
+        return n-999;
     }
 }

09250d0b5171886dbf67e20aa24ff5d0289e84a8
Bharath <bharathummadi4@gmail.com>
2026-09-10 23:25:14 +0530

    Time: 1 ms (99.99%) | Memory: 45.4 MB (73.23%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -14,26 +14,24 @@
  * }
  */
 class Solution{
+    List<List<Integer>>ans=new ArrayList<>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum){
-        List<List<Integer>>ans=new ArrayList<>();
-        if(root==null){
-            return ans;
-        }
-        DFS(root,targetSum,new ArrayList<>(),ans);
+        DFS(root,targetSum,new ArrayList<>());
         return ans;
     }
-    public static void DFS(TreeNode root,int targetSum,List<Integer>list,List<List<Integer>>ans){
+    void DFS(TreeNode root,int target,List<Integer>list){
         if(root==null){
             return;
         }
-        list.add(root.val);
-        if(targetSum==root.val && root.left==null && root.right==null){
+        if(root.val==target && root.left==null && root.right==null){
+            list.add(root.val);
             ans.add(new ArrayList<>(list));
             list.remove(list.size()-1);
             return;
         }
-        DFS(root.left,targetSum-root.val,list,ans);
-        DFS(root.right,targetSum-root.val,list,ans);
+        list.add(root.val);
+        DFS(root.left,target-root.val,list);
+        DFS(root.right,target-root.val,list);
         list.remove(list.size()-1);
     }
 }

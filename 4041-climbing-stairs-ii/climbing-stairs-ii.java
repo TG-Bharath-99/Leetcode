da0e8701770a8377402027474dd03cad2a5fbe03
Bharath <bharathummadi4@gmail.com>
2026-09-11 16:37:16 +0530

    Time: 62 ms (5.18%) | Memory: 134.7 MB (5.64%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,15 +1,31 @@
 class Solution{
-    public int climbStairs(int n, int[] cost){
-        int f=0;
-        int s=cost[0]+1;
-        if(n==1) return s;
-        int t=cost[1]+Math.min(4,s+1);
-        for(int i=3;i<=n;i++){
-            int x=cost[i-1]+Math.min(f+9,Math.min(s+4,t+1));
-            f=s;
-            s=t;
-            t=x;
+    int fun(int []dp,int n,int []cost){
+        if(dp[n]!=-1) return dp[n];
+        if(n==0) return 0;
+        if(n==1) return cost[n-1]+fun(dp,n-1,cost)+1;
+        if(n==2){
+            return dp[n]=cost[n-1]+Math.min(fun(dp,n-1,cost)+1,fun(dp,n-2,cost)+4);
         }
-        return t;
+        if(n==3){
+            return dp[n]=cost[n-1]+Math.min(Math.min(fun(dp,n-1,cost)+1,fun(dp,n-2,cost)+4),fun(dp,n-3,cost)+9);
+        }
+        
+        dp[n]=cost[n-1]+Math.min(Math.min(fun(dp,n-1,cost)+1,fun(dp,n-2,cost)+4),fun(dp,n-3,cost)+9);
+        return dp[n];
+    }
+    public int climbStairs(int n, int[] cost){
+        int []dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        if(n==0) return 0;
+        if(n==1) return cost[n-1]+fun(dp,n-1,cost)+1;
+        if(n==2){
+            return dp[n]=cost[n-1]+Math.min(fun(dp,n-1,cost)+1,fun(dp,n-2,cost)+4);
+        }
+        if(n==3){
+            return dp[n]=cost[n-1]+Math.min(Math.min(fun(dp,n-1,cost)+1,fun(dp,n-2,cost)+4),fun(dp,n-3,cost)+9);
+        }
+        fun(dp,n,cost);
+        return dp[n];
     }
 }
